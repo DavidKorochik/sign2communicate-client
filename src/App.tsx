fb17f8e21eb213bcdb90790a2e8f54db199566bc
DavidKorochik <david1korochik@gmail.com>
2022-04-02 07:11:00 +0300

    Set up react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import AppLayout from './components/layout/Layout';
 import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
+import { Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <>
-      <AppLayout>
-        <Signup />
-      </AppLayout>
+      <Routes>
+        <AppLayout>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+        </AppLayout>
+      </Routes>
     </>
   );
 };
