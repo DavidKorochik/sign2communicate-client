9a35663c58e209573b8e9ba07e9c83a3e3b46dee
DavidKorochik <david1korochik@gmail.com>
2022-04-02 06:56:46 +0300

    Imported the signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import AppLayout from './components/layout/Layout';
+import Signup from './components/signup/Signup';
 
 const App: React.FC = () => {
   return (
     <>
       <AppLayout>
-        <div></div>
+        <Signup />
       </AppLayout>
     </>
   );
