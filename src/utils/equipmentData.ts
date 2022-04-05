e3124b3a3a70f333849dc55dc38fdfa1b14f9314
DavidKorochik <david1korochik@gmail.com>
2022-04-05 08:46:42 +0300

    Added the equipment data

---
+++ src/utils/equipmentData.ts
@@ -0,0 +1,34 @@
+export const equipmentData = [
+  {
+    title: '624',
+    value: '624',
+  },
+  {
+    title: '709',
+    value: '709',
+  },
+  {
+    title: 'רמ"ק',
+    value: 'רמ"ק',
+  },
+  {
+    title: 'אנטנה',
+    value: 'אנטנה',
+  },
+  {
+    title: 'מגבר',
+    value: 'מגבר',
+  },
+  {
+    title: 'עמו"ד',
+    value: 'עמו"ד',
+  },
+  {
+    title: 'מכלול 120',
+    value: 'מכלול 120',
+  },
+  {
+    title: 'מכלול 121',
+    value: 'מכלול 121',
+  },
+];
