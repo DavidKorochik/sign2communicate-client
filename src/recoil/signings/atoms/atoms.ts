d260946fb6bddf8edc61818bfeb8a5fdc20e7d8e
DavidKorochik <david1korochik@gmail.com>
2022-04-07 07:31:20 +0300

    Added a loading state and a selector to get the signings from the db

--- src/recoil/signings/atoms/atoms.ts
+++ src/recoil/signings/atoms/atoms.ts
@@ -1,5 +1,4 @@
 import { atom } from 'recoil';
-// import moment from 'moment';
 import type { ISigning } from '../../../interfaces/signing/types';
 
 export const signingState = atom<ISigning[]>({
@@ -15,32 +14,7 @@ export const signingState = atom<ISigning[]>({
   ],
 });
 
-// export const signingEquipmentState = atom<string[]>({
-//   key: 'signingEquipmentState',
-//   default: [],
-// });
-
-// export const signingDateState = atom<moment.Moment | null>({
-//   key: 'signingDateState',
-//   default: null,
-// });
-
-// export const returnDateState = atom<moment.Moment | null>({
-//   key: 'returnDateState',
-//   default: null,
-// });
-
-// export const signingTimeState = atom<moment.Moment | null>({
-//   key: 'signingTimeState',
-//   default: null,
-// });
-
-// export const signingDescriptionState = atom<string>({
-//   key: 'signingDescriptionState',
-//   default: '',
-// });
-
-// export const loadingState = atom<boolean>({
-//   key: 'loadingState',
-//   default: true,
-// });
+export const loadingState = atom<boolean>({
+  key: 'loadingState',
+  default: true,
+});
--- src/recoil/signings/selectors/selectors.ts
+++ src/recoil/signings/selectors/selectors.ts
@@ -1,3 +1,14 @@
 import { selector } from 'recoil';
+import axios from 'axios';
 
-export const getSignings = selector({});
+export const getSignings = selector({
+  key: 'getSignings',
+  get: async ({ get }) => {
+    try {
+      const res = await axios.get('/api/signings');
+      return res.data;
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  },
+});
