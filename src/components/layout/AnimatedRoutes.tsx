39afc6562cb65ca33de38be349ba4541a31d1a4f
DavidKorochik <david1korochik@gmail.com>
2022-04-08 17:47:30 +0300

    Added the private route wrapper

--- src/components/layout/AnimatedRoutes.tsx
+++ src/components/layout/AnimatedRoutes.tsx
@@ -3,6 +3,7 @@ import Login from '../../components/login/Login';
 import Home from '../../components/home/Home';
 import AddSigning from '../form/AddSigning';
 import SigningsList from '../list/SigningsList';
+import PrivateRoute from '../../utils/PrivateRoute';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
@@ -18,8 +19,22 @@ function AnimatedRoutes() {
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/create' element={<AddSigning />} />
-        <Route path='/signings' element={<SigningsList />} />
+        <Route
+          path='/create'
+          element={
+            <PrivateRoute>
+              <AddSigning />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/signings'
+          element={
+            <PrivateRoute>
+              <SigningsList />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </AnimatePresence>
   );
