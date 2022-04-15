121eaa33651f1d30d80c1f607b2248ae22c79bb4
DavidKorochik <david1korochik@gmail.com>
2022-04-16 00:32:53 +0300

    Modified the add signing function

--- src/utils/signings/recoilFunctions.ts
+++ src/utils/signings/recoilFunctions.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import type { ISigning } from '../../interfaces/signing/types';
 
 export const addSigning = async (signing: ISigning) => {
@@ -10,6 +10,15 @@ export const addSigning = async (signing: ISigning) => {
 
   try {
     const res = await axios.post('/api/signing', signing, config);
+    return res.data;
+  } catch (err: any) {
+    console.error(err.response.data.error);
+  }
+};
+
+export const getSignings = async () => {
+  try {
+    const res = await axios.get('/api/signing');
     return res.data;
   } catch (err: any) {
     console.error(err.response.data.error);
