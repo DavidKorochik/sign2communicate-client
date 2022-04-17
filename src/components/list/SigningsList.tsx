dacd909443b969ae234986eb020229c47ee3a7f8
DavidKorochik <david1korochik@gmail.com>
2022-04-17 17:02:26 +0300

    Added a spinner while signings are fetching

--- src/components/list/SigningsList.tsx
+++ src/components/list/SigningsList.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { getSignings } from '../../utils/signings/recoilFunctions';
+import { loadingState } from '../../recoil/signings/atoms/atoms';
+import { useRecoilState } from 'recoil';
 import { Row } from 'antd';
 import Signing from '../signing/Signing';
 import type { ISigning } from '../../interfaces/signing/types';
@@ -7,38 +9,55 @@ import { motion } from 'framer-motion';
 import { pageAnimation } from '../../utils/animations';
 import moment from 'moment';
 import './SigningsList.css';
+import Spinner from '../../utils/spinner/Spinner';
 
 const SigningsList: React.FC = () => {
   const [signingsListState, setSigningsListState] = useState<ISigning[]>([]);
+  const [loading, setLoading] = useRecoilState<boolean>(loadingState);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     (async () => {
       const res = await getSignings();
       setSigningsListState(res);
     })();
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1700);
   }, []);
 
   return (
-    <motion.div
-      className='card-wrapper'
-      transition={{ type: 'linear' }}
-      variants={pageAnimation}
-      initial={pageAnimation.hidden}
-      animate={pageAnimation.enter}
-      exit={pageAnimation.exit}
-    >
-      <Row>
-        {signingsListState.map((signing) => (
-          <div style={{ margin: '40px' }}>
-            <Signing
-              key={signing.id}
-              description={signing.description}
-              signingDate={moment(signing.signingDate).format('DD/MM/YYYY')}
-            />
-          </div>
-        ))}
-      </Row>
-    </motion.div>
+    <>
+      {loading ? (
+        <>
+          <Spinner />
+        </>
+      ) : (
+        <motion.div
+          className='card-wrapper'
+          transition={{ type: 'linear' }}
+          variants={pageAnimation}
+          initial={pageAnimation.hidden}
+          animate={pageAnimation.enter}
+          exit={pageAnimation.exit}
+        >
+          <Row>
+            {signingsListState.map((signing) => (
+              <div style={{ margin: '40px' }}>
+                <Signing
+                  key={signing.id}
+                  description={signing.description}
+                  signingDate={moment(signing.signingDate).format('DD/MM/YYYY')}
+                  returningDate={moment(signing.returningDate).format(
+                    'DD/MM/YYYY'
+                  )}
+                />
+              </div>
+            ))}
+          </Row>
+        </motion.div>
+      )}
+    </>
   );
 };
 
